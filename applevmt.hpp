29a8f79f6af8e30364b70fff5382bdcfca8becb7
ApplePi19 <22938155+ApplePi19@users.noreply.github.com>
2018-09-14 02:06:08 +0300

    Fixed estimate length
    Inconsistent results

--- applevmt.hpp
+++ applevmt.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <Windows.h>
 #include <cstdint>
 #include <cstddef>
 #include <cstring>
@@ -10,15 +11,22 @@ private:
 	uintptr_t* original_tbl = nullptr;
 	uintptr_t* new_tbl = nullptr;
 	size_t estimated_funcs = 0;
+	
+	void estimate_length(std::uintptr_t* vftbl_start,std::size_t &length)
+	{
+		MEMORY_BASIC_INFORMATION meme_info = { 0 };
+		do
+		{
+			length++;
+			VirtualQuery(reinterpret_cast<const void*>(vftbl_start[length]), &meme_info, sizeof(meme_info));
+		} while (meme_info.Protect == PAGE_EXECUTE_READ || meme_info.Protect == PAGE_EXECUTE_READWRITE);
+	}
 public:
 	void init(void* class_ptr)
 	{
 		this->table_ptr = static_cast<uintptr_t**>(class_ptr);
 		this->original_tbl = *this->table_ptr;
-
-		while (this->original_tbl[this->estimated_funcs])
-			this->estimated_funcs++;
-
+		this->estimate_length(this->original_tbl, this->estimated_funcs);
 		this->new_tbl = new uintptr_t[sizeof(uintptr_t)*this->estimated_funcs];
 		memcpy(this->new_tbl, this->original_tbl, sizeof(uintptr_t)*this->estimated_funcs);
 		*this->table_ptr = this->new_tbl;
